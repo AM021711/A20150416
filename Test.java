730d2ba51db7eaa37831cbf1ae4fc3d581fc169a
AM021711 <z0919888300@gmail.com>
2015-04-16 16:47:05 +0800

    AM021711

---
+++ Test.java
@@ -0,0 +1,10 @@
+﻿import java.util.*
+public class Test {
+	public static void main (String[] args) {
+		System.out.println ("資工二A");
+
+		System.out.println ("AM021711");
+
+		System.out.println ("李昱緯");
+	}
+}
